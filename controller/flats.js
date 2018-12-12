cc93e44e7d39636a30fc5a1f62c071ce4fbe6795
gergo <pap133@gmail.com>
2018-12-12 16:30:56 +0100

    comments

--- controller/flats.js
+++ controller/flats.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const flats = express();
-const models = require('../models');
-const Sequelize = require('sequelize');
+const express = require('express'); // importáljuk az express app-ot
+const flats = express(); // definiájuk az express app-ot
+const models = require('../models'); // importáljuk a modeleket
+const Sequelize = require('sequelize'); // importáljuk a sequelize-t
 
 //index
-flats.get('/', (req, res) => {
-  models.Flat.findAll().then(tasks => {
-    res.json(tasks);
+flats.get('/', (req, res) => {  // flats object-re meghívunk egy http get metódust, egyik bemeneti argumentuma a route, a másik egy callback függvény
+  models.Flat.findAll().then(tasks => { // models-ből a Flat modelre meghivunk egy findAll metódust, hogy az összes Flat elemet kipörgesse
+    res.json(tasks);  // json formátumban visszaadjuk az adatot
   });
 });
 
 //show
-flats.get('/:id', (req, res) => {
+flats.get('/:id', (req, res) => { // flats object-re meghívunk egy http get metódust, egyik bemeneti argumentuma a route, a másik egy callback függvény
   models.Flat.findById(req.params.id).then(flats => {
     res.json(flats);
   });
 });
 
 //create
-flats.post('/', (req, res) => {
-  models.Flat.create({
-    title: req.body.title,
-    price: req.body.price,
+flats.post('/', (req, res) => {   // flats object-re meghívunk egy http post metódust, egyik bemeneti argumentuma a route, a másik egy callback függvény
+  models.Flat.create({ // models-ből a Flat objectre meghivunk egy create metódust, hogy létrehozzunk egy uj rekordot
+    title: req.body.title, // a mező nevet a request body-ból bodyparser segitsegevel kapja meg
+    price: req.body.price, // több kommentre nem maradt idő :(
     floorArea: req.body.floorArea,
     country: req.body.country,
     zip: req.body.titlereq.body.zip,
